d2ee3cf60e72d7e5e95785bb6a0eacf3f87bfa30
karachevtsev <karachevtsev@yahoo.com>
2024-09-09 22:12:55 +0300

    Updated lecture-4 (namespace alias example)

--- typescript/04-modules/examples/01-namespaces/02-aliases.ts
+++ typescript/04-modules/examples/01-namespaces/02-aliases.ts
@@ -1,3 +1,8 @@
+// Псевдонім
+// Можливо, нам доводиться створювати безліч об’єктів Shapes.Complex.Image(),
+// але щоразу набирати повне ім’я класу з урахуванням просторів імен, ймовірно,
+// не всім сподобатися, особливо коли модулі мають глибоку вкладеність за принципом матрьошки.
+// Щоб скоротити обсяг коду, ми можемо використовувати псевдоніми, що задаються ключовим словом import .
 namespace Shapes {
     export class Circle {
         constructor() {
@@ -11,6 +16,7 @@ namespace Shapes {
         }
     }
 
+    // Вкладені простори імен
     export namespace Complex {
         export class Image {
             constructor() {
@@ -31,3 +37,8 @@ import complex = Shapes.Complex;
 
 const img1 = new complex.Image();
 const img2 = new Shapes.Complex.Image(); // те саме, що і 30 рядок
+
+// або
+import shapeImg = Shapes.Complex.Image;
+
+const img3 = new shapeImg();
