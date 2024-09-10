1945e4885fc36accc5aa3938899359de347e5857
karachevtsev <karachevtsev@yahoo.com>
2024-09-10 12:20:11 +0300

    Updated Lecture-4 examples

--- typescript/04-modules/examples/04-modules-sample/main.ts
+++ typescript/04-modules/examples/04-modules-sample/main.ts
@@ -1,27 +1,27 @@
 // Після компіляції ts файли будуть перетворені js
 // для роботи прикладів у tsconfig.json потрібно встановити "module": "es6"
-import { Validator, ValidationResult } from './validator.js';
+import {Validator, ValidationResult} from './validator.js';
 
 const data1 = {
-  firstName: 'Ivan',
-  lastName: 'Ivaonv',
-  age: 25,
-  email: 'ivanov@example.com',
+    firstName: 'Ivan',
+    lastName: 'Ivaonv',
+    age: 25,
+    email: 'ivanov@example.com',
 };
 
 const data2 = {
-  firstName: 'Ivan',
-  lastName: '',
-  age: 'qwe',
-  email: 'example',
+    firstName: 'Ivan',
+    lastName: '',
+    age: 'qwe',
+    email: 'example',
 };
 
 // налаштування об'єкта для перевірки
 const config = {
-  firstName: 'required',
-  lastName: 'required',
-  age: 'number',
-  email: 'email',
+    firstName: 'required',
+    lastName: 'required',
+    age: 'number',
+    email: 'email',
 };
 
 const validator = new Validator(config);
